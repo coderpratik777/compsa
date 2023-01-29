34cc2034058f240998c36afd38df6c47e14fae60
pratikrpawar <pratikrpawar321@gmail.com>
2023-01-29 22:42:20 +0530

    new pdates

---
+++ src/components/animatingcircle.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles/circleanimation.css"
+
+export default function Circle()
+{
+    return(
+        
+<div class="view">
+  <div class="plane main">
+    <div class="circle"></div>
+    <div class="circle"></div>
+    <div class="circle"></div>
+    <div class="circle"></div>
+    <div class="circle"></div>
+    <div class="circle"></div>
+  </div>
+</div>
+    )
+}
--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Footer from './footer'
 import './styles/main.css'
+import Circle from './animatingcircle'
+import img1 from '../images/brooke-cagle-g1Kr4Ozfoac-unsplash.jpg'
 
 export default function Main()
 {
@@ -10,7 +12,9 @@ export default function Main()
             <div className="binaryrunning">
                 hiii
             </div>
-            ::after==$0
+            <div className="cirlceanimation">
+                <Circle/>
+            </div>
             <div className="navbar">
                 <div className="center">
                     <ul>
@@ -24,9 +28,12 @@ export default function Main()
                 </div>
             </div>
             <div className="logo"></div>
-            <div className="title">
+            <div className="title ">
                 COMPUTER SCIENCE AND ENGINEERING 
-                DEPARTMENT
+                ASSOCIATION
+            </div>
+            <div className="tagline">
+            &gt;&gt; “I think everybody in this country should learn how to program a computer because it teaches you how to think.”- Steve Jobs
             </div>
         </div>
         
@@ -37,6 +44,30 @@ export default function Main()
              &gt;&gt; Some of the glimpse of the COMPSA department
            </div>
            <div className="container2">
+            <div className="info">
+                <div className="info-title">
+                    What is COMPSA ?</div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit pretium scelerisque molestie a, aptent fringilla tempus nullam inceptos duis ullamcorper facilisi suscipit nunc, diam placerat porttitor felis sem suspendisse bibendum est pulvinar mollis.Praesent rhoncus montes morbi eleifend semper maecenas eu phasellus bibendum, quam scelerisque natoque leo ipsum odio cras facilisis, aliquet ex risus pulvinar posuere hac in pretium.Inceptos sem dolor tellus ipsum risus id penatibus accumsan aliquet fermentum adipiscing eros class, tempor pretium proin elit natoque nisi facilisis dictum nibh feugiat est pellentesque.In ut viverra tristique nisi nostra diam feugiat sollicitudin facilisi semper penatibus duis est finibus nibh, cursus vulputate imperdiet parturient mollis justo auctor leo hac sit nulla sapien potenti.Tincidunt integer hendrerit augue ullamcorper leo convallis condimentum eu hac commodo neque dignissim vel a, aenean venenatis donec tristique vulputate facilisis phasellus sociosqu potenti finibus egestas non.Elit risus ullamcorper sollicitudin aliquet sagittis dictum adipiscing praesent vitae conubia ut lorem est, luctus convallis dapibus purus nisi per eleifend amet tempor aenean sapien.Penatibus porttitor fermentum imperdiet at gravida platea nunc posuere mus vitae enim massa pulvinar </p>                
+            </div>
+            
+           </div>
+        </div>
+        <div className="main2">
+           <div className="blur_circle"></div>
+           <div className="blur_circle blur_circle2"></div>
+           <div className="container2">
+            <div className="info">
+                <div className="info-title">
+                    Glimpse of the COMPSA Activities</div>
+                   <div className="galleryimages" >
+                    <img src={img1}  alt="logo"/>
+                    <img src={img1}  alt="logo"/>
+                    <img src={img1}  alt="logo"/>
+                    <img src={img1}  alt="logo"/>
+                    <img src={img1}  alt="logo"/>
+                    <img src={img1}  alt="logo"/>
+                   </div>
+            </div>
             
            </div>
         </div>
